eb59d37b662eabd4593ba208c78762843bd5d3a5
kimberlyle <kimberly_le@berkeley.edu>
2013-11-06 13:19:27 -0800

    New dataframe for SAPP package for Analyzers

---
+++ sapp.R
@@ -0,0 +1,34 @@
+install.packages("spatstat")
+library(spatstat)
+install.packages("SAPP")
+library(sapp)
+
+# Examples
+data(main2003JUL26)
+head(main2003JUL26)
+
+### GOALS: Generate a dataframe for the SAPP Package 
+
+### DATAFRAME FOR SAPP ==========================================================================
+
+cleandata <-read.csv("./data-curators/CleanData1938-2013.csv")
+
+### Notice cleandata["HH.mm.SS.ss"] does not contain real numbers,  by looking at jap.quakes,  we need to create a time variable
+numbers <- c(1:nrow(cleandata))
+long <-cleandata[["LON"]]
+lat <-cleandata[["LAT"]]
+magnitude <-cleandata[["MAG"]]
+
+times <-cleandata[["HH.mm.SS.ss"]]
+datatime <-as.POSIXct(strptime(paste(dates, times), "%Y/%m/%d %H:%M:%OS"))
+newtime <-as.numeric(difftime(datatime,datatime[1],units="days"))
+
+depth <- cleandata[["DEPTH"]]
+year <- as.numeric(strftime(datatime, format="%Y"))
+month <- as.numeric(strftime(datatime, format="%m"))
+day <- as.numeric(strftime(datatime, format="%d"))
+
+sappdf = data.frame(no.= numbers,longitude=long,latitude=lat,magnitude=magnitude,
+           time=newtime,depth=depth,year=year,month=month,day=day)
+
+head(sappdf)
