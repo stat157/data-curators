d438ed163b24e96cef2eb00f92475e62f15c8357
tristantao <tao.tristan@gmail.com>
2013-11-05 19:31:43 -0800

    curate_util now has the option to do datetime conversion, added better usage

--- curate_util_usage.py
+++ curate_util_usage.py
@@ -6,15 +6,20 @@ import curate_util as CU
 
 if __name__ == "__main__":
 
+    #Simple Version
     catalog_dict = CU.grab_data_dict(1998,1999, 'clean_data/')
     data_frame  = CU.grab_data_frame(catalog_dict)
-    print data_frame
+    print data_frame.tail(10)
 
-    catalog_dict = CU.grab_data_dict(1998,1998, 'clean_data/')
-    data_frame  = CU.grab_data_frame(catalog_dict)
-    print data_frame
+    #Not asking to also return datetime row.
+    catalog_dict = CU.grab_data_dict(1998,1999, 'clean_data/')
+    data_frame  = CU.grab_data_frame(catalog_dict, convert_datetime=True)
+    print data_frame.head(10)
 
+    #Sample on actually converting numbers to float and working with them.
     catalog_dict = CU.grab_data_dict(1999,1999, 'clean_data/')
     data_frame  = CU.grab_data_frame(catalog_dict)
-    print data_frame
+    data_frame['MAG'].apply(float)
+    average = sum(data_frame['MAG'])/float(len(data_frame['MAG']))
+    print "Average Magnitude in 1999: %s" % average
 
