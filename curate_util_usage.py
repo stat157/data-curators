02ecae75d21d32b0d4eea03e93441d051ee9daca
tristantao <tao.tristan@gmail.com>
2013-11-12 21:32:57 -0800

    Also added new data, with magnitude modified

--- curate_util_usage.py
+++ curate_util_usage.py
@@ -32,3 +32,10 @@ if __name__ == "__main__":
     df_250 = data_frame.ix[rows]
     df_250.to_csv(os.path.join(os.getcwd(), "250.csv"), index = False)
     print df_250
+
+    #return magnitude > 3.5 from 1930 to 2013 as available
+    catalog_dict = CU.grab_data_dict(1932,2013, 'clean_data/')
+    data_frame = CU.grab_data_frame(catalog_dict, minimum_magnitude=3.5)
+    sorted_data_frame = data_frame.sort_index(by=['YYYY/MM/DD'], ascending=[True])
+    sorted_data_frame.to_csv(os.path.join(os.getcwd(), "1932_2013_mag_3.5.csv"), index = False)
+
